162781e9a90e59c0cbd0c0ba8b4a927055af1420
Kris Chery <kris.chery2@gmail.com>
2016-06-05 15:09:48 -0500

    made changes to application lay out, updated the routings, added the flashes through redenring, did a little bit of testing but need to acquire the updated versions testing framework

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,31 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "user should enter a first name" do
+    user = User.new
+    assert !user.save
+    assert !user.errors[:first_name].empty?
+  end
+
+  test "user should enter a last name" do
+    user = User.new
+    assert !user.save
+    assert !user.errors[:last_name].empty?
+  end
+
+  test "user should enter a profile name" do
+    user = User.new
+    assert !user.save
+    assert !user.errors[:profile_name].empty?
+  end
+
+  test "a user should have a unique name " do
+    user = User.new
+    user.profile_name = users(:steen).profile_name
+    assert !user.save
+    puts user.errors.inspect
+    assert !user.errors[:profile_name].empty?
+  end
+
 end
