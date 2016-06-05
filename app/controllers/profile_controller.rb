22438416eea7655733d1ece7f76cd539f9d04d19
Kris Chery <kris.chery2@gmail.com>
2016-06-05 16:35:02 -0500

    Final updates and added Profile for users and their statuses

---
+++ app/controllers/profile_controller.rb
@@ -0,0 +1,12 @@
+class ProfileController < ApplicationController
+
+  def show
+    @user = User.find_by_profile_name(params[:id])
+    if @user
+      @statuses = @user.statuses.all
+      render action: :show
+    else
+    render file: 'public/404', status: 404, formats: [:html] # this code is the user_id isn't found.
+    end
+  end
+end
